dc9dfad9d31589fec08a70177d2683e3482e1f16
mslysz <macslysz@gmail.com>
2023-06-13 01:49:48 +0100

    added js animation

--- main.js
+++ main.js
@@ -34,3 +34,23 @@ lightbox.addEventListener('click', e => {
   if (e.target !== e.currentTarget) return
   lightbox.classList.remove('active');
 })
+
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = 'In November 2022 I decided that I needed to change something in my professional life, my choice was to become a programmer. I had long been interested in creating simple, basic websites.My choice was to learn how to create websites and web applications.I started with online courses, from YouTube, Udemy.I am currently doing a Software developer course at the Pitman School based in Edinburgh.I am trying to progress as quickly as possible and get the best knowledge possible.In the near future I want to get employed as a Front - End - Developer.'
+
+let indexText = 0;
+const time = 40;
+
+const addLetter = () => {
+  spnText.textContent += txt[indexText];
+  indexText++;
+  if (indexText == txt.length) clearInterval(indexTyping);
+}
+
+const cursorAnimation = () => {
+  spnCursor.classList.toggle('active')
+}
+
+const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
